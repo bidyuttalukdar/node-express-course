a75f7d31b64338c7d69675c0db91cfab3ec34fa6
Bidyut Talukdar <bidyut3004@gmail.com>
2024-07-17 22:53:26 +0530

    Add not found middleware

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -6,24 +6,28 @@ require('dotenv').config();
 const tasks = require('./routes/task');
 const connectDB = require('./db/connect');
 
+//Middleware 
+const notFound = require('./middleware/notFound');
 
 
-//Middleware
 
-// to get the data into JSON
+// to parse the data
 app.use(express.json());
 app.use(express.static("./public"));
 
 
+//Middleware
+app.use(notFound)
+
 // Routes
 app.use('/api/v1/tasks', tasks);
 
-const PORT  = process.env.PORT || 9000
+const PORT = process.env.PORT || 9000
 
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI)
-        app.listen(PORT, ()=>{
+        app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`)
         })
     } catch (error) {
